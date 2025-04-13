ef23a94f225fa70ad902e5cd82a2e780dc6d0b52
lukejianu <71729102+lukejianu@users.noreply.github.com>
2025-04-13 15:42:04 -0400

    Client: Replace junk with PixiJS example

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,24 +1,15 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import * as PIXI from 'pixi.js';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const app = new PIXI.Application();
+await app.init({ width: 800, height: 600 })
+document.body.appendChild(app.canvas);
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+await PIXI.Assets.load('sprite.png');
+let sprite = PIXI.Sprite.from('sprite.png');
+app.stage.addChild(sprite);
+
+let elapsed = 0.0;
+app.ticker.add((ticker) => {
+  elapsed += ticker.deltaTime;
+  sprite.x = 100.0 + Math.cos(elapsed/50.0) * 100.0;
+});
