8a911e47de1ec8e2766076ddd67aecc44cc8ccbd
lukejianu <71729102+lukejianu@users.noreply.github.com>
2025-08-14 16:37:08 -0700

    Fix broken server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,6 @@
 import { WebSocketServer, WebSocket } from "ws";
 import { initServer, runTick } from "./app";
+import { Id, Position } from "../../common/src/data";
 
 const ticksPerSecond = 5;
 const timeStepMs = 1000 / ticksPerSecond;
@@ -7,8 +8,8 @@ const port = 12345
 
 const main = () => {
   const wss = new WebSocketServer({ port });
-  const gs: Map<number, number> = new Map()
-  const conns: Array<WebSocket> = new Array()
+  const gs: Map<Id, Position> = new Map()
+  const conns: Map<Id, WebSocket> = new Map()
   initServer(wss, gs, conns);
   setInterval(() => runTick(gs, conns), timeStepMs)
 }
