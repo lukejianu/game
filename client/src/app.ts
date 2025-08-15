1387c1581172304d3b81bbe788ef067303b69c41
lukejianu <71729102+lukejianu@users.noreply.github.com>
2025-08-15 11:33:12 -0700

    Start restructuring client for better testability

--- client/src/app.ts
+++ client/src/app.ts
@@ -1,9 +1,13 @@
 import * as PIXI from "pixi.js";
 
+import { Id, Position } from "../../common/src/data";
+
 const CANVAS_WIDTH = 400;
 const CIRCLE_RADIUS = 20;
 const MY_CIRCLE_COLOR = 'red';
 const OTHER_CIRCLE_COLOR = 'blue'; // TODO: Use this.
+
+export type ClientGameState = Map<Id, Position>; // TODO: Use this.
 
 export const connectToServer = (): WebSocket => {
   const gameServerPort = 12345;
@@ -21,15 +25,42 @@ export const initPixi = async (): Promise<PIXI.Application> => {
   return pixi;
 };
 
-export const handleMsg = (gameState: Map<string, number>, msg: MessageEvent<any>) => {
+/**
+ * NOTE: If there is more than one message in the queue, then something is
+ * wrong. This is because this function is called more often than the server's
+ * function that sends messages (new states). Thus, the only way there can be
+ * more than one message in the queue is due to a poor network connection. In
+ * this case, it's might be good to speed up the tick speed of the client so
+ * that it can catch up, as opposed to always taking the most recent new state.
+ * For now, the function will use the most recent state and will log if it
+ * has more than one.   
+ * 
+ * NOTE: This function's job is to interpolate entities. Interpolation is
+ * necessary to smooth the behavior of other entities, since there is always a
+ * gap between receiving states from the server. During time t to t + 1, we
+ * simulate what *likely* happened between time t - 1 and t. This means that
+ * this function needs to be called many times in the interval [t - 1, t]. It
+ * also needs to know where in the interval its being called (how far through).
+ */
+export const processMessages = (gs: ClientGameState, msgQueue: Array<MessageEvent<any>>) => {
+  if (msgQueue.length != 1) {
+    console.warn(`Message queue has ${msgQueue.length} elements!`)
+  }
+  for (const msg of msgQueue) {
+    handleMsg(gs, msg)
+  }
+  msgQueue.length = 0
+}
+
+const handleMsg = (gs: ClientGameState, msg: MessageEvent<any>) => {
     const obj = JSON.parse(msg.data)
-    gameState.clear()
+    gs.clear()
     for (const key in obj) {
-      gameState.set(key, obj[key])
+      gs.set(parseInt(key, 10), obj[key])
     }
 }
 
-export const drawGameState = (gs: Map<string, number>, pixi: PIXI.Application) => {
+export const drawGameState = (gs: ClientGameState, pixi: PIXI.Application) => {
   pixi.stage.removeChildren();
   gs.forEach((pos, _id) => drawCircle(pos, MY_CIRCLE_COLOR, pixi));
 }
@@ -40,14 +71,20 @@ const drawCircle = (x: number, color: PIXI.FillInput, pixi: PIXI.Application) =>
   pixi.stage.addChild(gfx);
 }
 
-export const registerKeybinds = (socket: WebSocket) => {
+export const registerKeybinds = (gs: ClientGameState, socket: WebSocket) => {
   window.addEventListener("keydown", (e) => {
     const key = e.key.toLowerCase();
     if (["a", "d"].includes(key)) {
       socket.send(key)
+      handleInput(gs, key)
     }
   });
 }
+
+export const handleInput = (gs: ClientGameState, key: string) => {
+  // TODO: Predict.
+}
+
 
 // TODO: Delete.
 export function add(a: number, b: number) {
--- client/src/main.ts
+++ client/src/main.ts
@@ -1,4 +1,4 @@
-import { connectToServer, drawGameState, handleMsg, initPixi, registerKeybinds } from "./app";
+import { connectToServer, drawGameState, processMessages, initPixi, registerKeybinds, ClientGameState } from "./app";
 
 const ticksPerSecond = 5;
 const timeStepMs = 1000 / ticksPerSecond;
@@ -6,13 +6,15 @@ const timeStepMs = 1000 / ticksPerSecond;
 const main = async () => {
   const pixi = await initPixi();
   const conn = connectToServer();
-  registerKeybinds(conn);
-  const gameState: Map<string, number> = new Map();
+  const gs: ClientGameState = new Map();
+  const msgQueue: Array<MessageEvent<any>> = new Array();
+  registerKeybinds(gs, conn);
   conn.onmessage = (msg) => {
-    handleMsg(gameState, msg);
+    msgQueue.push(msg);
   };
   setInterval(() => {
-    drawGameState(gameState, pixi);
+    processMessages(gs, msgQueue);
+    drawGameState(gs, pixi);
   }, timeStepMs)
 }
 
